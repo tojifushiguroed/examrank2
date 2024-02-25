0c4e328d7184471d2e812037ea1eea9d3ba42264
tojifushiguroed <116516982+tojifushiguroed@users.noreply.github.com>
2024-02-25 16:19:34 +0300

    Add files via upload

---
+++ level2/union.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   union.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egolboyu <egolboyu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 13:40:21 by egolboyu          #+#    #+#             */
+/*   Updated: 2024/02/25 16:18:14 by egolboyu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int check(int c, char *str, int index)
+{
+	int i = 0;
+	
+	while(i < index)
+	{
+		if (str[i] == c)
+			return 0;
+		i++;
+	}
+	return  1;
+}
+
+int main(int argc, char **argv)
+{	
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	
+	if (argc == 3)
+	{
+		while(argv[1][i] != '\0')
+		{
+			i++;
+		}
+		while(argv[2][j] != '\0')
+		{
+			argv[1][i] = argv[2][j];
+			i++;
+			j++;
+		}
+		i--;
+		while(k <= i)
+		{
+			if(check(argv[1][k], argv[1], k) == 1) 
+				write (1, &argv[1][k], 1); 
+			k++;
+		}
+	}
+	write (1, "\n", 1);
+}
